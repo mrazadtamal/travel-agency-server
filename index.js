0448e3e40996861c80255971f290b2529eced246
mrazadtamal <mrazadt@gmail.com>
2022-01-28 11:38:10 +0600

    50%

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const app = express();
 const dotenv = require("dotenv");
 const cors = require("cors");
 const { MongoClient, ObjectId } = require("mongodb");
+const bodyParser = require("body-parser");
 const port = process.env.PORT || 5000;
 
 dotenv.config();
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const uri = process.env.MONGO_URL;
 
